027fb70f5d337e7b7fcb42c176c919ee1a6bfac6
Leoo955 <josueleonard9533@gmail.com>
2026-06-17 13:47:39 +0200

    chore: clean up temporary scripts and test files

--- test_render.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  const output = execSync('curl -s -w "%{http_code}" http://192.168.3.237:3005/cards', { encoding: 'utf-8' });
-  console.log("Output:", output.substring(output.length - 10)); // Just the status code and end of html
-} catch (e) {
-  console.error("Error:", e.message);
-}
