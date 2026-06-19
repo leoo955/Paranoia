5af419f532be17c4e1f19f4f59a7f51464923273
Leoo955 <josueleonard9533@gmail.com>
2026-06-19 14:45:58 +0200

    fix(security): CSP for discord, pin Github Actions, fix npm vulns

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; img-src 'self' blob: data: https://www.transparenttextures.com https://i.imgur.com https://minotar.net; connect-src 'self' https://vitals.vercel-insights.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; img-src 'self' blob: data: https://www.transparenttextures.com https://i.imgur.com https://minotar.net https://cdn.discordapp.com https://vzge.me; connect-src 'self' https://vitals.vercel-insights.com;",
           },
         ],
       },
